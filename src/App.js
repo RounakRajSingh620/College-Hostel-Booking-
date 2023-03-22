4011d9ab1b0d2fde7d64a0fd3501d85b510b3ec5
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2023-03-22 19:38:47 +0530

    Hostel-Booking-System

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Header from './Header';
 
 import Welcome from './Welcome';
 import './App.css';
+import Detail from './Detail';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Welcome/>
+      <Detail/>
     </div>
   );
 }
---
+++ src/Detail.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Detail.css"
+function Detail() {
+    return (
+        <div className="Abouts">
+        <h2 className="About_Acc"> All About Student Accommodation & Facilities </h2>
+        <h3 className="points">We, at SRM Institute of Science and Technology (formerly known as SRM University), provide a wide choice in selecting the type or rooms in a hostel to suit every budget, with an aim of providing a decent stay and the best possible learning environment.</h3>
+        <h3 className="points">The quality of life is one of the most important factors considered by the students deciding to stay in the College/University hostels. By quality, it is not just the rooms but also the overall environment of the University campus. Ours is one of the greenest Campuses’ in the country with all the facilities that are expected from global Universities. The stay at our campus provides a wealth of experience by developing qualities such as camaraderie and social responsibility. Our students comprise a cosmopolitan mix from all parts of India as well as many countries across the globe. </h3>
+        <h3 className="points">The students staying at SRM hostels are provided with healthy and sumptuous food options at highly subsidized rates. They can choose to have North Indian or South Indian menu in the dining halls that are co-located. For the benefit of our international students, the hostels also offer a multi-cuisine dining facility providing Thai, Chinese and continental food.</h3>
+        <h3 className="points">Life on campus helps the students not only to study but also to socialize with the peers. Friendships formed on the campus last a lifetime. SRM provides a comfortable and safe housing to all its students. The students housed in the campus are encouraged to use the gymnasium, playground, and all indoor gaming facilities.</h3>
+        </div>
+    )
+}
+
+export default Detail
