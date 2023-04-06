add629a35327ea9efca43738c8bdac73bdc629b5
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2023-04-06 22:55:21 +0530

    College-Hostel-Booking

--- src/Footer.js
+++ src/Footer.js
@@ -3,12 +3,12 @@ import "./Footer.css"
 function Footer() {
   return (
     <footer className="footer" >
-      <p className='Text'>Copyrights All Rights Reserved</p>
+      <p >Copyrights All Rights Reserved</p>
       <span className='social_items'>
-        <a className='items' href="https://www.facebook.com/SRMUniversityOfficial" ><img src="https://cdn-icons-png.flaticon.com/512/2504/2504903.png" alt="facebook"/></a>
-        <a className='items' href="https://twitter.com/SRM_Univ"><img src="https://cdn-icons-png.flaticon.com/512/3670/3670127.png" alt="twitter"/></a>
-        <a className='items' href="https://www.instagram.com/SRMUniversityOfficial/"><img src="https://www.flaticon.com/free-icon/instagram_3955024" alt="instagram"/></a>
-        <a className='items' href="https://www.linkedin.com/company/srm-ist-chennai"><img src="https://www.flaticon.com/free-icon/linkedin_1377213" alt="linkedin"/></a>
+        <a className='items' href="https://www.facebook.com/SRMUniversityOfficial" ><img src="https://cdn-icons-png.flaticon.com/512/2504/2504903.png" alt="facebook" /></a>
+        <a className='items' href="https://twitter.com/SRM_Univ"><img src="https://cdn-icons-png.flaticon.com/512/3670/3670127.png" alt="twitter" /></a>
+        <a className='items' href="https://www.instagram.com/SRMUniversityOfficial/"><img src="https://cdn-icons-png.flaticon.com/512/4138/4138124.png" alt="instagram" /></a>
+        <a className='items' href="https://www.linkedin.com/company/srm-ist-chennai"><img src="https://cdn-icons-png.flaticon.com/512/2504/2504923.png" alt="linkedin" /></a>
       </span>
     </footer>
   )
