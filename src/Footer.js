4f84f8aa3a1d846b1431cbeb8ec7cfaaad48912e
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2023-04-06 21:37:03 +0530

    College-Hostel-Booking

--- src/Footer.js
+++ src/Footer.js
@@ -3,10 +3,13 @@ import "./Footer.css"
 function Footer() {
   return (
     <footer className="footer" >
-      <p className="text-center">
-        Copyright &copy; srmist.com
-      </p>
-
+      <p className='Text'>Copyrights All Rights Reserved</p>
+      <span className='social_items'>
+        <a className='items' href="https://www.facebook.com/SRMUniversityOfficial">Facebook</a>
+        <a className='items' href="https://twitter.com/SRM_Univ">Twitter</a>
+        <a className='items' href="https://www.instagram.com/SRMUniversityOfficial/">Instagram</a>
+        <a className='items' href="https://www.linkedin.com/company/srm-ist-chennai">LinkedIn</a>
+      </span>
     </footer>
   )
 }
